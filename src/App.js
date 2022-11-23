fbbfbbb3568d9d974bdeb1efb281fe16220572d5
Keiryan Wilson <keiryanwilson@gmail.com>
2022-11-22 19:47:05 -0500

    removed unused code

--- src/App.js
+++ src/App.js
@@ -6,39 +6,15 @@ import {
   ButtonOne,
   ButtonTwo,
 } from "./app.styles";
-import Button from "./Button/button";
 
 function App() {
   return (
     <Container>
       <LightHalf>
-        <ButtonOne
-          gradient={{
-            deg: "90deg",
-            stops: [
-              "rgba(131,58,180,1) 0%",
-              "rgba(253,29,29,1) 50%,",
-              "rgba(252,176,69,1) 100%),",
-            ],
-          }}
-        >
-          Hover me!
-        </ButtonOne>
+        <ButtonOne>Hover me!</ButtonOne>
       </LightHalf>
       <DarkHalf>
-        <ButtonTwo
-          gradient={{
-            deg: "90deg",
-            stops: [
-              "rgba(131,58,180,1) 0%",
-              "rgba(253,29,29,1) 50%,",
-              "rgba(252,176,69,1) 100%),",
-            ],
-          }}
-          text=""
-        >
-          Hover me!
-        </ButtonTwo>
+        <ButtonTwo>Hover me!</ButtonTwo>
       </DarkHalf>
     </Container>
   );
